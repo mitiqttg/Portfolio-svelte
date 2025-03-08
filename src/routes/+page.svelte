d244267ffe0dee948ea343749803c8cd0c9e4343
mitiqttg <minhtien2812000@gmail.com>
2025-03-08 10:05:10 +0200

    modied 3.14

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p class="text-red-400">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
