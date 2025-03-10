90d7ea8c0b8cf13a2b5ac0cde69f9db86a4da5a6
mitiqttg <minhtien2812000@gmail.com>
2025-03-10 10:03:18 +0200

    update new

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p class="text-red-400">Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+    import Main from "../components/Main.svelte";
+</script>
+
+<Main />
